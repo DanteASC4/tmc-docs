bfa90f7c5a412865c2d062fdbba3d05d8930c77f
danteasc4 <zexovon@gmail.com>
2026-05-21 12:27:52 -0400

    Update all the examples to use new stuff

--- components/staticchart.tsx
+++ components/staticchart.tsx
@@ -8,10 +8,12 @@ import {
 
 type FlatDataOptions = { data: number[] } & Record<string, any>;
 type NestedDataOptions = { data: number[][] } & Record<string, any>;
+type LineDataOptions = { data: number[] | number[][] } & Record<string, any>;
 
 type ChartOutputProps =
   | { type: 'barchart' | 'donutchart' | 'piechart'; options: FlatDataOptions }
-  | { type: 'barchartStacked' | 'linechart'; options: NestedDataOptions };
+  | { type: 'barchartStacked'; options: NestedDataOptions }
+  | { type: 'linechart'; options: LineDataOptions };
 
 const availableCharts = {
   barchart,
@@ -27,13 +29,15 @@ export function StaticChart(props: ChartOutputProps) {
 
   if (type === 'barchart' || type === 'donutchart' || type === 'piechart') {
     svgString = availableCharts[type](options);
-  } else if (type === 'barchartStacked' || type === 'linechart') {
+  } else if (type === 'barchartStacked') {
+    svgString = availableCharts[type](options);
+  } else if (type === 'linechart') {
     svgString = availableCharts[type](options);
   }
 
   return (
     <div
-      className="p-4 border border-gray-700 rounded-md bg-black text-white flex justify-center w-full overflow-x-auto"
+      className="p-4 [&_svg]:border [&_svg]:border-gray-700 [&_svg]:rounded-md bg-black text-white flex justify-center w-full overflow-x-auto"
       dangerouslySetInnerHTML={{ __html: svgString }}
     />
   );
